6a975ee276921e474f52298fdfac450e74b90f43
Leonard Mandtler <leonard.mandtler@bertelsmann-stiftung.de>
2022-11-22 16:26:30 +0100

    apply changes for workshop

--- models/mart/customers.sql
+++ models/mart/customers.sql
@@ -10,12 +10,6 @@ orders as (
 
 ),
 
-payments as (
-
-    select * from {{ ref('stg_payments') }}
-
-),
-
 customer_orders as (
 
         select
@@ -30,21 +24,6 @@ customer_orders as (
 
 ),
 
-customer_payments as (
-
-    select
-        orders.customer_id,
-        sum(amount) as total_amount
-
-    from payments
-
-    left join orders on
-         payments.order_id = orders.order_id
-
-    group by orders.customer_id
-
-),
-
 final as (
 
     select
@@ -54,15 +33,11 @@ final as (
         customer_orders.first_order,
         customer_orders.most_recent_order,
         customer_orders.number_of_orders,
-        customer_payments.total_amount as customer_lifetime_value
 
     from customers
 
     left join customer_orders
         on customers.customer_id = customer_orders.customer_id
-
-    left join customer_payments
-        on  customers.customer_id = customer_payments.customer_id
 
 )
 
